5e4b95d7232f1d757be423b427c664f8f1d8a211
Saurabh Goyal <sgoyalsmvj@gmail.com>
2023-05-10 07:47:14 +0530

    intial commit

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,41 +1,5 @@
-import { useEffect,useRef } from "react"
+const TopPlay = () => (
+  <div>TopPlay</div>
+);
 
-import { Link } from "react-router-dom"
-import { useSelector,useDispatch } from "react-redux"
-import {Swiper,SwiperSlide} from 'swiper/react';
-import { FreeMode } from "swiper";
-import 'swiper/css'
-import 'swiper/css/free-mode'
-
-import PlayPause from './PlayPause'
-
-import {playPause,setActiveSong} from '../redux/features/playerSlice';
-import {useGetTopChartsQuery, useTopChartsQuery} from '../redux/services/shazamCore';
-
-
-
-const TopPlay = () => {
-  useEffect(()=>{
-    divRef.current.scrollIntoView({behavior:'smooth'});
-  });
-  const dispatch = useDispatch();
-  const {activeSong,isPlaying} = useSelector((state)=>state.player);
-
-  const {data}  = useGetTopChartsQuery();
-  const divRef = useRef(null);
-  const topPlays = data?.slice(0,5);
-  const handlePauseClick = ()=>{
-
-  }
-  const handlePlayClick = ()=>{
-    dispatch(setActiveSong({song,data,i}));
-    dispatch(playPause(false));
-  }
-  return (
-    <div ref={divRef} className='xl:ml-6 ml-0 xl:mb-0  mb-6 flex-1 xl:max-w-500px max-w-full flex flex-col'>
-      <div className=""></div>
-    </div>
-  )
-}
-
-export default TopPlay
+export default TopPlay;
