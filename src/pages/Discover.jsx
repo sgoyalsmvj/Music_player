f5e8b22a2459957c09adb53fa1c905c8b325eb34
Saurabh Goyal <sgoyalsmvj@gmail.com>
2023-05-16 00:11:58 +0530

    top-charts

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,53 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazam";
+import { useDispatch, useSelector } from "react-redux";
+// import {chartData} from '../assets/data'
+const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  // const data = chartData;
+  const genreTitle = "Pop";
+
+  // console.log(data.songs);
+  if (isFetching) return <Loader title="loading songs" />;
+  if (error) return <Error />;
+  return (
+    <div className="flex flex-col ">
+      {/* //wrapper for title and select */}
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="fond-bold text-3xl text-white texxt-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* wrapper for displaying songs. */}
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data.tracks?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            index={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
