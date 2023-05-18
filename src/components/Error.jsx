763e26afcd797e1be79fe379ec6308f8d4992cce
Saurabh Goyal <sgoyalsmvj@gmail.com>
2023-05-18 23:37:55 +0530

    clean

--- src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Error = () => (
-  <div className='w-full flex justify-center items-center'>
-    <h1 className="font-bold text-2xl">Something Gone Wrong </h1>
-  </div>
-);
-
-export default Error;
